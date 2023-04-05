8f7f33e63b4e67b973c50103929c531158bab9c1
Matt Haneburger <115722743+matthewHaneburgerSnyk@users.noreply.github.com>
2023-04-05 12:56:39 -0400

    concat_strings.cpp
    adding xss to cpp code

--- practice/concat_strings.cpp
+++ practice/concat_strings.cpp
@@ -5,4 +5,21 @@ int main() {
   std::string second = "World";
   std::string buffer = first + second;
   std::cout << buffer << "\n";
+  
+  std::vector<int> x {3,2,1,4,5,6};
+    vectorSubrange<int> xs(x,2,3);
+    std::vector<int>::iterator i = begin(xs);
+    std::cout << *i << " " << *(i+2) << std::endl;
+    
+    constVectorSubrange<int> xss(x,2,-1);
+    std::vector<int>::const_iterator it = begin(xss);
+    std::cout << *it << " " << *(it+2) << std::endl;
+
+    for ( auto ii=begin(xss); ii!=end(xss); ++ii)
+        std::cout << *ii << ",";
+    std::cout << std::endl;
+
+    for ( auto ii : xss )
+        std::cout << ii << ",";
+    std::cout << std::endl;
 }
